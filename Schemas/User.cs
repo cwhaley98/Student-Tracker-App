4f400b59b997320fb4865680b301bce975d61bcd
cwhaley98 <c.whaley98@outlook.com>
2026-03-14 00:45:45 -0400

    Added login/create account feature. Implements a user creation functionality to log into the app

---
+++ Schemas/User.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Tracker_App.Schemas
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Unique] // Ensure usernames are unique
+        public string Username { get; set; }
+
+        public string PasswordHash { get; set; } // Store hashed passwords for security, not plain text
+    }
+}
